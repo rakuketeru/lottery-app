04c6dbd7571e2c53e59a3556185c7246793a6ef6
rakuketeru <101056300+rakuketeru@users.noreply.github.com>
2025-04-30 10:47:00 +0900

    Update _supabase.js

--- api/_supabase.js
+++ api/_supabase.js
@@ -1,7 +1,6 @@
-// _supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://nwytzenfzgakwjxtrors.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53eXR6ZW5memdha3dqeHRyb3JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NzEyOTYsImV4cCI6MjA2MTU0NzI5Nn0.SooZmrQQ-_ISKDdxZGa2awJAMBPeHAazIdB-TOYtH4U'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
